9acc508adcb22d31d528b892c23d3d6cccaebf05
panthiwa <thiwab59@gmail.com>
2019-02-10 09:36:24 +0700

    test

---
+++ MaidRegisterController.java
@@ -0,0 +1,112 @@
+package sut.se.g18.Controller;
+
+import sut.se.g18.Repository.*;
+import org.springframework.http.MediaType;
+import sut.se.g18.Entity.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import sut.se.g18.Model.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class MaidRegisterController {
+    @Autowired
+    private MaidRegisterRepository maidRegisterRepository;
+    @Autowired
+    private TitleNameRepository titleNameRepository;
+    @Autowired
+    private TypeworkingRepository typeworkingRepository;
+    @Autowired
+    private WorkingDateRepositoy workingDateRepositoy;
+    @Autowired
+    private CompanyRepository companyRepository;
+    @Autowired
+    private WelfareAndSalaryRepository welfareAndSalaryRepository;
+
+    public MaidRegisterController(MaidRegisterRepository maidRegisterRepository,
+            TitleNameRepository titleNameRepository, TypeworkingRepository typeworkingRepository,
+            WorkingDateRepositoy workingDateRepositoy,CompanyRepository companyRepository,WelfareAndSalaryRepository welfareAndSalaryRepository) {
+        this.maidRegisterRepository = maidRegisterRepository;
+        this.titleNameRepository = titleNameRepository;
+        this.typeworkingRepository = typeworkingRepository;
+        this.workingDateRepositoy = workingDateRepositoy;
+        this.companyRepository = companyRepository;
+        this.welfareAndSalaryRepository = welfareAndSalaryRepository;
+    }
+
+    @GetMapping(path = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<MaidRegisterEntity> Register() {
+        return maidRegisterRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+   
+    /* ======================================================= */
+    @GetMapping(path = "/title", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<TitleNameEntity> TitleName() {
+        return titleNameRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+    /* ======================================================= */
+
+    @GetMapping(path = "/typeworking", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<TypeworkingEntity> Typeworking() {
+        return typeworkingRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+
+    /* ======================================================= */
+    @GetMapping(path = "/workingDate", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<WorkingDateEntity> WorkingDate() {
+        return workingDateRepositoy.findAll().stream().collect(Collectors.toList());
+    }
+/* ======================================================= */
+    @GetMapping(path = "/ccompany", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<CompanyEntity> Company() {
+        return companyRepository.findAll().stream().collect(Collectors.toList());
+    }
+    /* ======================================================= */
+    @GetMapping(path = "/WelfareAndSalary", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<WelfareAndSalaryEntity> WelfareAndSalary() {
+        return welfareAndSalaryRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+    @PostMapping(path = "/regi/{titleSelect}/{inputmaidName}/{inputmaidPhone}/{inputmaidEmail}/{inputmaidAddress}/{inputdistrict}/{inputcanton}/{inputprovince}/{typeworkingEntity}/{workingDateEntity}/{companys}/{WelfareAndSalarySelect}")
+    public MaidRegisterEntity maidRegisterEntity(MaidRegisterEntity maidentity, @PathVariable String titleSelect,
+    @PathVariable String inputmaidName, @PathVariable String inputmaidPhone,
+    @PathVariable String inputmaidEmail, @PathVariable String inputmaidAddress,
+    @PathVariable String inputdistrict, @PathVariable String inputcanton ,
+    @PathVariable String inputprovince, @PathVariable String typeworkingEntity, @PathVariable String workingDateEntity,
+    @PathVariable String companys, @PathVariable String WelfareAndSalarySelect) {
+
+        MaidRegisterEntity maidRegisterEntity = new MaidRegisterEntity();
+        TitleNameEntity ti = titleNameRepository.findBytitlenameType(titleSelect);
+        CompanyEntity companyy = companyRepository.findBycompanyName(companys);
+        TypeworkingEntity type = typeworkingRepository.findByTypeworking(typeworkingEntity);
+        WorkingDateEntity work = workingDateRepositoy.findBytypeworkingDate(workingDateEntity);
+       WelfareAndSalaryEntity we = welfareAndSalaryRepository.findBywelsaName(WelfareAndSalarySelect);
+
+       maidRegisterEntity.setTitleNameEntity(ti);
+       maidRegisterEntity.setMaidName(inputmaidName);
+       maidRegisterEntity.setMaidAddress(inputmaidAddress);
+       maidRegisterEntity.setMaidEmail(inputmaidEmail);
+       maidRegisterEntity.setMaidPhone(inputmaidPhone);
+       maidRegisterEntity.setProvince(inputprovince);
+       maidRegisterEntity.setDistrict(inputdistrict);
+       maidRegisterEntity.setCanton(inputcanton);
+
+       maidRegisterEntity.setTypeworkingEntity(type);
+       maidRegisterEntity.setWorkingDateEntity(work);
+       maidRegisterEntity.setCompanyForMaid(companyy);
+       maidRegisterEntity.setWelfareAndSalaryEntity(we);
+
+        return maidRegisterRepository.save(maidRegisterEntity);
+    }
+
+}
---
+++ MaidRegisterEntity.java
@@ -0,0 +1,173 @@
+package sut.se.g18.Entity;
+
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@Table(name = "Register")
+public class MaidRegisterEntity {
+    @Id
+    @SequenceGenerator(name = "registerSeq", sequenceName = "registerSeq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "registerSeq")
+    @Column(name = "maidId", unique = true, nullable = false)
+    private @NonNull Long maidId;
+    @NotNull
+    private String maidName;
+    @NotNull
+    private String maidAddress;
+    @Email
+    private String maidEmail;
+    @NotNull
+    @Size(min = 10, max = 10)
+    @Pattern(regexp = "^0([0-9])+")
+    private String maidPhone;
+    @NotNull
+    private String province;
+    @NotNull
+    private String district;
+    @NotNull
+    private String canton;
+
+    // Many To One with Company
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = CompanyEntity.class)
+    private CompanyEntity companyForMaid;
+
+    // Many To One with TitleNameEntity
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = TitleNameEntity.class)
+    private TitleNameEntity titleNameEntity;
+
+    // Many To One with TypeworkingEntity
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = TypeworkingEntity.class)
+    private TypeworkingEntity typeworkingEntity;
+
+    // Many To One with WorkingDateEntity
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = WorkingDateEntity.class)
+    private WorkingDateEntity workingDateEntity;
+
+    // Many To One with WelfareAndSalaryEntity
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = WelfareAndSalaryEntity.class)
+    private WelfareAndSalaryEntity welfareAndSalaryEntity;
+
+
+
+    public Long getMaidId() {
+        return maidId;
+    }
+
+    public void setMaidId(Long maidId) {
+        this.maidId = maidId;
+    }
+
+    public String getMaidName() {
+        return maidName;
+    }
+
+    public void setMaidName(String maidName) {
+        this.maidName = maidName;
+    }
+
+    public String getMaidAddress() {
+        return maidAddress;
+    }
+
+    public void setMaidAddress(String maidAddress) {
+        this.maidAddress = maidAddress;
+    }
+
+    public String getMaidEmail() {
+        return maidEmail;
+    }
+
+    public void setMaidEmail(String maidEmail) {
+        this.maidEmail = maidEmail;
+    }
+
+    public String getMaidPhone() {
+        return maidPhone;
+    }
+
+    public void setMaidPhone(String maidPhone) {
+        this.maidPhone = maidPhone;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCanton() {
+        return canton;
+    }
+
+    public void setCanton(String canton) {
+        this.canton = canton;
+    }
+
+    public TitleNameEntity getTitleNameEntity() {
+        return titleNameEntity;
+    }
+
+    public void setTitleNameEntity(TitleNameEntity titleNameEntity) {
+        this.titleNameEntity = titleNameEntity;
+    }
+
+    public TypeworkingEntity getTypeworkingEntity() {
+        return typeworkingEntity;
+    }
+
+    public void setTypeworkingEntity(TypeworkingEntity typeworkingEntity) {
+        this.typeworkingEntity = typeworkingEntity;
+    }
+
+    public CompanyEntity getCompanyForMaid() {
+        return companyForMaid;
+    }
+
+    public void setCompanyForMaid(CompanyEntity companyForMaid) {
+        this.companyForMaid = companyForMaid;
+    }
+
+    public WorkingDateEntity getWorkingDateEntity() {
+        return workingDateEntity;
+    }
+
+    public void setWorkingDateEntity(WorkingDateEntity workingDateEntity) {
+        this.workingDateEntity = workingDateEntity;
+    }
+
+    /**
+     * @return the welfareAndSalaryEntity
+     */
+    public WelfareAndSalaryEntity getWelfareAndSalaryEntity() {
+        return welfareAndSalaryEntity;
+    }
+
+    /**
+     * @param welfareAndSalaryEntity the welfareAndSalaryEntity to set
+     */
+    public void setWelfareAndSalaryEntity(WelfareAndSalaryEntity welfareAndSalaryEntity) {
+        this.welfareAndSalaryEntity = welfareAndSalaryEntity;
+    }
+
+}
